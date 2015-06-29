d0f848adb77cc28cec282cfb373a41f787602036
Vasilij Belosludcev <bupy765@gmail.com>
2015-06-29 10:55:28 +0500

    Update _form.php

--- views/manager/_form.php
+++ views/manager/_form.php
@@ -10,42 +10,22 @@ use yii\helpers\Url;
 /* @var $model bupy7\pages\models\Page */
 /* @var $form yii\widgets\ActiveForm */
 
-$module = Yii::$app->getModule('pages');
-
 $form = ActiveForm::begin();
+
 echo $form->field($model, 'title')->textInput(['maxlength' => 255]);
+
 echo $form->field($model, 'alias')->textInput(['maxlength' => 255]);
+
 echo $form->field($model, 'published')->checkbox();
-$settings = [
-    'lang' => Yii::$app->language,
-    'minHeight' => 200,
-    'plugins' => [
-        'fullscreen',
-    ],
-];
-if ($module->addImage || $module->uploadImage) {
-    $settings['plugins'][] = 'imagemanager';
-}
-if ($module->addImage) {
-    $settings['imageManagerJson'] = Url::to(['images-get']);
-}
-if ($module->uploadImage) {
-    $settings['imageUpload'] = Url::to(['image-upload']);
-}
-if ($module->addFile || $module->uploadFile) {
-    $settings['plugins'][] = 'filemanager';
-}
-if ($module->addFile) {
-    $settings['fileManagerJson'] = Url::to(['files-get']);
-}
-if ($module->uploadFile) {
-    $settings['fileUpload'] = Url::to(['file-upload']);
-}
+
 echo $form->field($model, 'content')->widget(Imperavi::className(), [
     'settings' => $settings,
 ]);
+
 echo $form->field($model, 'title_browser')->textInput(['maxlength' => 255]);
+
 echo $form->field($model, 'meta_keywords')->textInput(['maxlength' => 200]);
+
 echo $form->field($model, 'meta_description')->textInput(['maxlength' => 160]);
 ?>
 <div class="form-group">
