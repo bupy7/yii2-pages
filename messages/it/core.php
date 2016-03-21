7749c44fe84262aff0f254ae8d60d98f7ee6816b
Lorenzo Milesi <maxxer@yetopen.it>
2016-03-21 23:13:03 +0100

    Italian translation

---
+++ messages/it/core.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Language 'IT'.
+ * 
+ * @author Lorenzo Milesi http://lorenzo.mile.si
+ * @version 1.0.0
+ */
+return [
+    'ID' => 'ID',
+    'TITLE' => 'Titolo',
+    'ALIAS' => 'Alias',
+    'PUBLISHED' => 'Pubblicata',
+    'CONTENT' => 'Contenuto',
+    'TITLE_BROWSER' => 'Titolo del browser',
+    'META_KEYWORDS' => 'Meta keyword',
+    'META_DESCRIPTION' => 'Meta description',
+    'CREATED_AT' => 'Creata il',
+    'UPDATED_AT' => 'Modificata il',
+    'PAGE_NOT_FOUND' => 'La pagina richiesta non esiste.',
+    'CREATE' => 'Crea',
+    'UPDATE' => 'Modifica',
+    'MODULE_NAME' => 'Pagine',
+    'SAVE_SUCCESS' => 'Salvataggio eseguito con successo!',
+    'DELETE_SUCCESS' => 'Cancellazione eseguita con successo!',
+];
+
+
