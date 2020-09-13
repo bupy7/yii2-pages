8cff939f66b89f32a7c7e61dea8c1e1bfe78f2f5
thanhtt <thanhtt@hybrid-technologies.co.jp>
2020-09-13 10:23:02 +0900

    Add Japanese translation.

---
+++ src/messages/ja/core.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Language 'JA'.
+ *
+ * @author Tran Trung Thanh https://github.com/umbalaconmeogia
+ * @since 1.0.0
+ */
+return [
+    'ID' => 'ID',
+    'TITLE' => 'タイトル',
+    'DISPLAY_TITLE' => '表示タイトル',
+    'ALIAS' => 'アリアス',
+    'PUBLISHED' => '公開',
+    'CONTENT' => 'コンテンツ',
+    'TITLE_BROWSER' => 'ブラウザタイトル',
+    'META_KEYWORDS' => 'メタキーワード',
+    'META_DESCRIPTION' => 'メタディスクリプション',
+    'CREATED_AT' => '作成日時',
+    'UPDATED_AT' => '更新日時',
+    'PAGE_NOT_FOUND' => '要求されたページが存在しません。',
+    'CREATE' => '作成',
+    'UPDATE' => '編集',
+    'MODULE_NAME' => 'ページ',
+    'SAVE_SUCCESS' => '保存が完了しました！',
+    'DELETE_SUCCESS' => '削除が完了しました！',
+];
